f1088a639e1d212e816c7bbe32aab02b7916618a
Tail_Straw <maeva.gr@live.fr>
2024-11-06 22:20:45 +0100

    Notion-11 Add game management elements

--- src/pages/GamesManager/GamesManager.jsx
+++ src/pages/GamesManager/GamesManager.jsx
@@ -1,11 +1,26 @@
-import  React, { useState } from "react";
+import  React, { useState, useEffect } from "react";
 import PageHeader from "../../components/PageHeader/pageHeader";
+import Card from "../../components/Card/card";
 import GameForm from "../../components/Form/gameForm";
 import chevronIcon from "../../assets/images/icons/chevron-up.svg";
+import { readGames } from "../../utils/gameManagerClient";
 
 function GamesManager() {
     const [open, setOpen] = useState(false);
+    const [games, setGames] = useState(null);
+    
+    useEffect(() => {
+        async function getGames() {
+            const games = await readGames();
 
+            if (games) {
+                setGames(games);
+            }
+        }
+
+        getGames();
+    });
+    
     return (
         <React.Fragment>
             <PageHeader
@@ -17,10 +32,29 @@ function GamesManager() {
                     }
                 ] }
             />
-            <section className={ open ? "game-manager-section open" : "game-manager-section" }>
-                <h2 className="game-manager-section-title" onClick={ collapse }>Nouveau jeu<img src={ chevronIcon } className="game-manager-section-title-icon"/></h2>
-                <GameForm/>
-            </section>
+            <article className="game-manager">
+                <section className="game-manager-index">
+                    <h2 className="game-manager-index-title">Nos jeux</h2>
+                    <ul className="game-manager-index-gallery">
+                        { games ? games.map((game, index) => {
+                            return (
+                                <li className="game-manager-index-gallery-item" key={ "game-" + index }>
+                                    <Card
+                                        title={ game.name }
+                                        media={ game.media }
+                                        description={ game.description }
+                                        details={ game.rules }
+                                    />
+                                </li>
+                            );
+                        }) : "Aucun jeu" }
+                    </ul>
+                </section>
+                <section className={ open ? "game-manager-section open" : "game-manager-section" }>
+                    <h2 className="game-manager-section-title" onClick={ collapse }>Nouveau jeu<img src={ chevronIcon } className="game-manager-section-title-icon"/></h2>
+                    <GameForm/>
+                </section>
+            </article>
         </React.Fragment>
     )
 
